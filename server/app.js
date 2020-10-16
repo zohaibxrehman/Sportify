fe9d8de66184169aa648029696adf8cd6e757e3a
zohaibxrehman <zohaibxrehman@gmail.com>
2020-10-16 18:36:29 -0400

    SPOR-8 Add post user data route.

--- server/app.js
+++ server/app.js
@@ -1,13 +1,47 @@
-var admin = require('firebase-admin');
+const firebase = require('firebase-admin');
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
 const port = 3000
 
+app.use(bodyParser.json());
+
+const serviceAccount = require("./sportify-8d62e-firebase-adminsdk-sgz9l-882a9b2759.json");
+
+const config = {
+  apiKey: "AIzaSyC57XIbeDE6hgHYLulW7Pcyy-OHvnKEnu4",
+  credential: firebase.credential.cert(serviceAccount),
+  authDomain: "sportify-8d62e.firebaseapp.com",
+  databaseURL: "https://sportify-8d62e.firebaseio.com",
+  storageBucket: "sportify-8d62e.appspot.com"
+};
+firebase.initializeApp(config);
+
+const database = firebase.database();
+
 app.get('/', (req, res) => {
-  res.send('I am the sportify server.')
+  res.send('I am the sportify server.');
 })
 
 app.get('/login', (req, res) => {
+  res.sendStatus(200);
+})
+
+app.post('/user/profile', (req, res) => {
+  const postData = {
+    utorid : req.body.utorid,
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    birthdate: req.body.birthdate,
+    bio: req.body.bio,
+    sportsInterests: req.body.sportsInterests,
+    favoriteTeam: req.body.favoriteTeam,
+  };
+
+  var updates = {};
+  updates['/users/' + postData.utorid] = postData;
+  firebase.database().ref().update(updates);
+
   res.sendStatus(200);
 })
 
@@ -15,9 +49,4 @@ app.listen(port, () => {
   console.log(`Sportify server listening at http://localhost:${port}`)
 })
 
-var serviceAccount = require("/Users/bhavikkothari/Desktop/Sportify/server/sportify-8d62e-firebase-adminsdk-sgz9l-882a9b2759.json");
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://sportify-8d62e.firebaseio.com"
-});
