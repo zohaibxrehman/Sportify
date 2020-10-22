78e1cc0786e6f8b804af5498b6d39662f2aa9889
Bhavik Kothari <bhavikkothari@Bhaviks-MacBook-Pro.local>
2020-10-21 21:49:44 -0400

    SPOR7: Implement Event Creation Page

--- flutter/sportify_app/lib/main.dart
+++ flutter/sportify_app/lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'screens/landing_page.dart';
+import 'screens/event_creation_page.dart';
 
 void main() {
   runApp(
     MaterialApp(
-      home: LandingPage(),
+      home: EventCreation(),
     ),
   );
 }
