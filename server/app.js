dbb09933aa356a4f8581f4a6b48c317143c03716
zohaibxrehman <zohaibxrehman@gmail.com>
2020-10-29 21:54:52 -0400

    Add get event messages route

--- server/app.js
+++ server/app.js
@@ -77,6 +77,7 @@ app.post('/event/new', (req, res) => {
     title: title,
     description: description,
     location: location,
+    messages: false
   };
 
   let eventUpdates = {};
@@ -102,6 +103,15 @@ app.get('/events', (req, res) => {
 
 app.get('/events/:id', (req, res) => {
   const eventRef = firebase.database().ref('/events/' + req.params.id);
+  eventRef.once('value').then(function(snapshot) {
+    return res.send(snapshot.val());
+  }).catch(function(error) {
+    return res.sendStatus(404);
+  });
+})
+
+app.get('/events/:id/messages', (req, res) => {
+  const eventRef = firebase.database().ref('/events/' + req.params.id + '/messages');
   eventRef.once('value').then(function(snapshot) {
     return res.send(snapshot.val());
   }).catch(function(error) {
