e3d653fcec27b4bd80815b6dea61de2a65b11af9
zohaibxrehman <zohaibxrehman@gmail.com>
2020-11-12 17:10:09 -0500

    Fixed get user route url

--- server/app.js
+++ server/app.js
@@ -52,7 +52,7 @@ app.post('/user/new', (req, res) => {
   return res.sendStatus(200);
 })
 
-app.get('/users/:id', (req, res) => {
+app.get('/user/:id', (req, res) => {
   const userRef = firebase.database().ref('/users/' + req.params.id);
   userRef.once('value').then(function(snapshot) {
     return res.send(snapshot.val());
