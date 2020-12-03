f3f8ba5a625cdea9f7d633a17352207d5ccfd08f
Gaurav Setia <gaurav.singh.setia@gmail.com>
2020-12-02 19:10:42 -0500

    SPOR-20 Authentication done using firebase authentication

--- flutter/sportify_app/lib/screens/landing_page.dart
+++ flutter/sportify_app/lib/screens/landing_page.dart
@@ -4,7 +4,8 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart';
 import 'dart:io';
 import 'package:dio/dio.dart';
-import 'package:sportify_app/screens/home_page.dart';
+import 'package:sportify_app/screens/login_screen.dart';
+import 'package:sportify_app/screens/registration_screen.dart';
 
 class LandingPage extends StatefulWidget {
   @override
@@ -15,47 +16,67 @@ class LandingPage extends StatefulWidget {
 
 class LandingPageState extends State<LandingPage> {
   String serverResponse = 'Server response';
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage("images/sportify_logo.jpeg"),
-            fit: BoxFit.cover,
-          ),
-        ),
-        child: Align(
-          alignment: Alignment.bottomCenter,
-          child: GestureDetector(
-            child: Container(
-              height: 60,
-              margin: EdgeInsets.symmetric(horizontal: 70, vertical: 30),
-              alignment: Alignment.center,
-              child: Text(
-                'Log In',
-                style: TextStyle(
-                  fontSize: 20,
-                  color: Colors.white,
-                ),
-              ),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(25),
-                color: Colors.pink,
-              ),
-            ),
-            onTap: () => {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => HomePage()),
-              ),
-              _makeGetRequest()
-            },
-          ),
+        body: Container(
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage("images/sportify_logo.jpeg"),
+          fit: BoxFit.cover,
         ),
       ),
-    );
+      child: Align(
+          alignment: Alignment.bottomCenter,
+          child: Row(
+            children: [
+              Container(
+                height: 60,
+                margin: EdgeInsets.symmetric(horizontal: 70, vertical: 30),
+                // alignment: Alignment.centerRight,
+                child: FlatButton(
+                  child: Text(
+                    "Login",
+                    style: TextStyle(
+                      fontSize: 25,
+                      color: Colors.white,
+                    ),
+                  ),
+                  color: Colors.deepOrangeAccent,
+                  onPressed: () => {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => LoginScreen()),
+                    ),
+                    _makeGetRequest()
+                  },
+                ),
+              ),
+              Container(
+                height: 60,
+                // alignment: Alignment.bottomCenter,
+                child: FlatButton(
+                  child: Text(
+                    "Register",
+                    style: TextStyle(
+                      fontSize: 25,
+                      color: Colors.white,
+                    ),
+                  ),
+                  color: Colors.deepOrangeAccent,
+                  onPressed: () => {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => RegistrationScreen()),
+                    ),
+                    _makeGetRequest()
+                  },
+                ),
+              )
+            ],
+          )),
+    ));
   }
 
   _makeGetRequest() async {
